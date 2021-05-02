d2b6a0ec807a6d7d52f76d1a36f81f58c4f0826c
Dschahannam <uwunoozles@gmail.com>
2021-05-02 17:36:12 +0200

    register InviteTeamMember

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,7 @@ import "./systems/hud/info";
 import "./systems/hud/weaponinfo";
 import "./systems/hud/speedometer";
 import "./systems/phone";
+import "./systems/phone/team/TeamLeaderApp";
 import "./systems/confirmation";
 
 import "./systems/clothing";
