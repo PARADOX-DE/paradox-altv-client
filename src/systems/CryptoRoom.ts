b56aa96e469704fc36d49403b5a5a0a0ee19a681
Dschahannam <uwunoozles@gmail.com>
2021-06-27 14:43:51 +0200

    Add CryptoRoom and loadServer event

---
+++ src/systems/CryptoRoom.ts
@@ -0,0 +1,17 @@
+import alt from 'alt-client';
+import game from 'natives';
+import EventController from '../controllers/EventController';
+
+class CryptoRoom {
+    constructor() {
+        EventController.onServer("Crypto::LoadServer", this.loadServer.bind(this));
+    }
+
+    loadServer(serverCount: number){
+        let interiorID = game.getInteriorAtCoords(1725.842, 4687.657, 17.54688);
+        game.activateInteriorEntitySet(interiorID, 'serverroom_' + serverCount + 'server');
+        game.refreshInterior(interiorID);
+    }
+}
+
+export default new CryptoRoom();
